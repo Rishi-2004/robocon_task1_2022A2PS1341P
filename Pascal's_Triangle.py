e7e4d56d7e0944fd11ba04fa18940af07240d9b0
Rishi-2004 <130762429+Rishi-2004@users.noreply.github.com>
2023-04-15 21:52:04 +0530

    Update Pascal's_Triangle.py

--- Pascal's_Triangle.py
+++ Pascal's_Triangle.py
@@ -1,13 +1,13 @@
-class PascalTriangleGenerator:
-    def generate_triangle(self, numRows):
+class Solution(object):
+    def generate(self, numRows):
         """
         :type numRows: int
         :rtype: List[List[int]]
         """
-        triangle = []
+        result = []
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1, i):
-                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-            triangle.append(row)
-        return triangle
+                row[j] = result[i-1][j-1] + result[i-1][j]
+            result.append(row)
+        return result
