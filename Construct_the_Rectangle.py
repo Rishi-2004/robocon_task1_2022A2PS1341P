023ddd70b0f30b080d5522bd389f73385f038cc6
Rishi-2004 <130762429+Rishi-2004@users.noreply.github.com>
2023-04-15 22:14:41 +0530

    Update Construct_the_Rectangle.py

--- Construct_the_Rectangle.py
+++ Construct_the_Rectangle.py
@@ -1,10 +1,10 @@
-class RectangleConstructor:
-    def construct_rectangle(self, area):
+lass Solution(object):
+    def constructRectangle(self, area):
         """
         :type area: int
         :rtype: List[int]
         """
-        sqrt_area = int(area ** 0.5)
+        sqrt_area = int(math.sqrt(area))
         for width in range(sqrt_area, 0, -1):
             if area % width == 0:
                 length = area // width
